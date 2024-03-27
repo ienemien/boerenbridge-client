3f44d2f88e5cab93e0727c541e222dbb7287cdcc
Ineke Vermeulen <inekevermeulen@gmail.com>
2024-03-27 15:19:20 +0100

    added validations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {HttpClientModule} from "@angular/common/http";
+import {ErrorStateMatcher, ShowOnDirtyErrorStateMatcher} from "@angular/material/core";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), importProvidersFrom(HttpClientModule),]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    importProvidersFrom(HttpClientModule),
+    {provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher}]
 };
--- src/app/pages/choose-tricks/choose-tricks.component.ts
+++ src/app/pages/choose-tricks/choose-tricks.component.ts
@@ -2,13 +2,35 @@ import {Component, OnInit} from '@angular/core';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatIconModule} from "@angular/material/icon";
 import {MatInputModule} from "@angular/material/input";
-import {FormArray, FormBuilder, ReactiveFormsModule} from "@angular/forms";
+import {
+  AbstractControl,
+  FormArray,
+  FormBuilder,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators
+} from "@angular/forms";
 import {MatButton, MatIconButton} from "@angular/material/button";
 import {NgForOf, NgIf} from "@angular/common";
 import {ActivatedRoute, Router, RouterModule} from "@angular/router";
 import {Round} from "../../model/round";
 import {Player} from "../../model/player";
 import {ScoreService} from "../../services/score.service";
+import {Trick} from "../../model/trick";
+
+/** The total of chosen tricks cannot be the same as the amount of cards */
+function totalChosenInvalid(nrOfCards?: number): ValidatorFn {
+
+  return (
+    control: AbstractControl,
+  ): ValidationErrors | null => {
+    const tricks = control.get('tricks');
+    const total = tricks?.value.map((trick: Trick) => trick.tricks).reduce((acc: number, cur: number) => acc + cur, 0);
+
+    return total === nrOfCards ? {nrChosenInvalid: true} : null;
+  };
+}
 
 @Component({
   selector: 'bbs-choose-tricks',
@@ -30,22 +52,23 @@ export class ChooseTricksComponent implements OnInit {
     tricks: this.formBuilder.array([]),
   });
 
-  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute, private router: Router, private scoreService: ScoreService) {
+  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute,
+              private router: Router, private scoreService: ScoreService) {
   }
 
   ngOnInit() {
-    // @ts-ignore
-    this.route.data.subscribe(({ round } : { round: Round}) => {
-      this.round = round;
-      for(const score of round.scores) {
+    this.route.data.subscribe((data) => {
+      this.round = data['round'] as Round;
+      for (const score of this.round?.scores) {
         this.tricks.push(this.formBuilder.group({
-          playerId: [score.player.id],
+          playerId: [score.player.id, Validators.required],
           playerName: [score.player.name],
-          tricks: [],
+          tricks: [undefined, [Validators.required, Validators.min(0), Validators.max(this.round.numberOfCards)]],
         }));
       }
       const playerIndex = (this.round?.roundNumber ?? 1) % this.players.length;
       this.dealerId = this.players[playerIndex - 1].id;
+      this.chooseTricksForm.addValidators(totalChosenInvalid(this.round?.numberOfCards));
     });
     this.route.queryParams.subscribe((params) => this.gameId = params['game']);
   }
@@ -59,10 +82,11 @@ export class ChooseTricksComponent implements OnInit {
   }
 
   onSubmit() {
-    if(this.gameId && this.round?.roundNumber) {
+    if (!this.chooseTricksForm.invalid && this.gameId && this.round?.roundNumber) {
       this.scoreService.saveChosenTricks(this.gameId, this.round?.roundNumber, this.tricks.value).subscribe(() => {
-        this.router.navigate(['/actual-tricks'],  { queryParams: { game: this.gameId, round: this.round?.roundNumber } });
+        this.router.navigate(['/actual-tricks'], {queryParams: {game: this.gameId, round: this.round?.roundNumber}});
       });
     }
   }
+
 }
